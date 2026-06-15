34c474ecd725827352077b25f8305cebe43c12a3
mirac <extvaue@mapfre.com.pe>
2026-06-15 13:57:58 -0500

    add initial implementation of client portal components and features

---
+++ src/app/features/clients/client-detail/client-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-detail',
+  imports: [],
+  templateUrl: './client-detail.html',
+  styleUrl: './client-detail.scss',
+})
+export class ClientDetail {}
---
+++ src/app/shared/components/loading-state/loading-state.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoadingState } from './loading-state';
+
+describe('LoadingState', () => {
+  let component: LoadingState;
+  let fixture: ComponentFixture<LoadingState>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [LoadingState],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(LoadingState);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
